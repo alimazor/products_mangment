8d444a3c11817857863e56215d3408e62d7694c7
alimazor <raja19981031>
2021-06-18 11:47:12 +0100

    Pageable

--- src/main/java/com/products_managment/web/ProductController.java
+++ src/main/java/com/products_managment/web/ProductController.java
@@ -1,9 +1,17 @@
 package com.products_managment.web;
 
 import com.products_managment.dao.ProductsDao;
+import com.products_managment.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -12,7 +20,13 @@ public class ProductController {
 
 
     @RequestMapping("/index")
-    private String index(){
+    private String index(Model model, @RequestParam(name = "page",defaultValue = "0")int p,@RequestParam(name = "size",defaultValue = "5") int s){
+
+        Page<Product> productList = productsDao.findAll(PageRequest.of(p,s));
+        model.addAttribute("productList",productList.getContent());
         return "index";
     }
+
+
+
 }
